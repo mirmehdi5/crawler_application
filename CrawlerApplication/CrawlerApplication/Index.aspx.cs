8f6abfe056acca0f96e924a4447dd65530a2a61a
mirmehdi5 <mirmohammedmehdi@gmail.com>
2017-12-24 16:01:39 +0530

    crawle

---
+++ CrawlerApplication/CrawlerApplication/Index.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+
+namespace CrawlerApplication
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        int MaxNewsSize =Convert.ToInt32(ConfigurationManager.AppSettings["MaxNewsSize"] != null ? Convert.ToString(ConfigurationManager.AppSettings["MaxNewsSize"]) : "25");
+        String Rstring;
+        String rootURL = "http://edition.cnn.com/";
+        string name_in_news = "donald";
+        List<string> finalLinks = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            startfunction();
+        }
+
+        public void startfunction()
+        {
+            List<string> Homepage_Links = readpages(rootURL); //main list
+            displaylinks(Homepage_Links);
+            List<string> hreflinks = getpages(Homepage_Links);
+            foreach (var item in hreflinks)
+            {
+                List<string> childlinks = readpages(item);
+                displaylinks(childlinks);
+                List<string> childhreflink = getpages(childlinks);
+            }
+        }
+        public List<string> GetNewLinks(string content, string tag)
+        {
+            Regex regexLink = new Regex("<\\s*" + tag + "[^>]*>(.*?)<\\s*/\\s*" + tag + ">");//regex to get all the anchor tags
+            List<string> newLinks = new List<string>();
+            foreach (var match in regexLink.Matches(content))
+            {
+                if (!newLinks.Contains(match.ToString()))
+                    newLinks.Add(match.ToString());
+            }
+            return newLinks;
+        }
+
+        public List<string> getpages(List<string> pagelinks)
+        {
+            List<string> Links = new List<string>();
+            foreach (var link in pagelinks)
+            {
+                string temp = link;
+                if (temp.Contains("href=\"/") && temp.ToLower().Contains(name_in_news))
+                {
+                    string[] split = temp.Split(new[] { "href=\"/" }, StringSplitOptions.None);//extract URL from the tag
+                    string[] split1 = split[1].Split(new[] { "\"" }, StringSplitOptions.None);
+                    temp = rootURL + split1[0];
+                    Links.Add(temp);
+                }
+            }
+            return Links;
+        }
+
+        public List<string> readpages(string URL)
+        {
+            WebRequest myWebRequest;
+            WebResponse myWebResponse;
+            myWebRequest = WebRequest.Create(URL);
+            myWebResponse = myWebRequest.GetResponse();//Returns a response from an Internet resource
+            Stream streamResponse = myWebResponse.GetResponseStream();//return the data stream from the internet and save it in the stream
+            StreamReader sreader = new StreamReader(streamResponse);//reads the data stream
+            Rstring = sreader.ReadToEnd();//reads it to the end
+            List<string> aLinks = GetNewLinks(Rstring, "a");//gets the links only
+            streamResponse.Close();
+            sreader.Close();
+            myWebResponse.Close();
+            return aLinks;
+        }
+
+        public void displaylinks(List<string> linkList)
+        {
+            foreach (var link in linkList)
+            {
+                string temp = link;
+                if (temp.Contains("href=\"/") && temp.ToLower().Contains(name_in_news))
+                {
+                    string[] split = temp.Split(new[] { "href=\"/" }, StringSplitOptions.None);//get URL for images
+                    temp = split[0] + "href=\"" + rootURL + split[1];
+                    string finalimageurls = string.Empty;
+                    string finalcontentdesciptiontemp = string.Empty;
+                    if (temp.Contains("src=\"//"))
+                    {
+                        string[] imagrurlstart = temp.Split(new[] { "src=\"//cdn.cnn.com" }, StringSplitOptions.None);
+                        string[] imageurlends = imagrurlstart[1].Split(new[] { "\"" }, StringSplitOptions.None);
+                        finalimageurls = "//cdn.cnn.com" + imageurlends[0];
+                    }
+                    else
+                    {
+                        finalimageurls = "/images/trump.jpg";
+                    }
+                    string[] finallistsplit = temp.Split(new[] { "href=\"" }, StringSplitOptions.None);
+                    string[] finallistsplit1 = finallistsplit[1].Split(new[] { "\"" }, StringSplitOptions.None);//extrat URL from the tag 
+                    string finallisttemp = finallistsplit1[0];
+                    if (temp.Contains("alt=\""))
+                    {
+                        string[] finalcontentdesciption = temp.Split(new[] { "alt=\"" }, StringSplitOptions.None);
+                        string[] finalcontentdesciption1 = finalcontentdesciption[1].Split(new[] { ". " }, StringSplitOptions.None);
+                        finalcontentdesciptiontemp = finalcontentdesciption1[0];
+                    }
+                    if (finalLinks.FindIndex(o => string.Equals(finallisttemp, o, StringComparison.OrdinalIgnoreCase)) < 0 && finalLinks.Count <= MaxNewsSize)
+                    {
+                        finalLinks.Add(finallisttemp);
+                        var lnkMicrosoft = new HyperLink //create Hyperlink Tag
+                        {
+                            Text = temp.ToString(),
+                            CssClass= ".img_class",
+                            ImageUrl = finalimageurls,
+                            NavigateUrl = finallisttemp
+                        };
+                        Panel_CNN.Controls.Add(lnkMicrosoft);
+                        var lnkMicrosofttext = new HyperLink
+                        {
+                            Text = finalcontentdesciptiontemp + ".... READ MORE...",
+                            NavigateUrl = finallisttemp
+                        };
+                        Panel_CNN.Controls.Add(lnkMicrosofttext);
+                        Panel_CNN.Controls.Add(new Literal() { Text = "<hr/>" });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CrawlerApplication/CrawlerApplication/obj/Release/Package/PackageTmp/Index.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="CrawlerApplication.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="scripts/jquery-1.12.0.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="scripts/tweetList.js"></script>
+</head>
+<body>
+    <div style="clear: both"></div>
+    <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#menu1" style="width: 101%;" onfocus="showmydiv_menu1()">CNN News</a></li>
+        <li><a data-toggle="tab" href="#menu2" style="width: 101%;" onfocus="showmydiv_menu2()">Twitter</a></li>
+
+    </ul>
+
+    <div id="menu1" class="tab-pane fade in active contact_details">
+        <form id="form1" runat="server">
+            <asp:Panel ID="Panel1" runat="server"></asp:Panel>
+        </form>
+    </div>
+    <div id="menu2" class="tab-pane fade contact_details">
+        <h2>Profile Example without using widget ID (New Twitter Change)</h2>
+        <div id="exampleProfile"></div>
+    </div>
+
+
+</body>
+</html>
